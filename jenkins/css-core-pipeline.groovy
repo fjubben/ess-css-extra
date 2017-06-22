834864f5bcc2c77cb9ff75089b31b69b183c8091
fjubben <jesperolsson@ci0011126.esss.lu.se>
2017-06-22 15:45:03 +0200

    autocommit

---
+++ jenkins/css-core-pipeline.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy​
+/*
+ * This file contains collects the similarities of the different pipelines
+ * in to methods that can be loaded in by the other main pipelines.
+ *
+ */
+{
+  def pleasework(trythis) {
+    echo "thank me it works:  $trythis"
+  }
+  return this
+}
